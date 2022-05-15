52f6d5c0b8117f084bf07fd4c3e1c30bb4b9ef9c
ROBSON DOS SANTOS DIAS <robiftm@gmail.com>
2022-05-15 19:00:53 -0300

    Add files via upload

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,19 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+	public static void main(String[] args) {
+		Livros livros = new Livros();
+		Gato gato = new Gato();
+			
+		System.out.println(gato);
+		System.out.println(livros);
+	}
+
+}
+
+class Livros{
+	private String nome;
+	private String npag;
+}
